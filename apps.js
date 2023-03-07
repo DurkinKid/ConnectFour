d5d9b63fe51a89c269aa5da253273b4cf6a0fb95
charley <charlesdurkin94@gmail.com>
2023-03-07 12:59:56 -0500

    reorganize consts and cpu thoughts

--- apps.js
+++ apps.js
@@ -1,35 +1,45 @@
 
 let winner;
-let choices;
-let scores;
-let beginGame = true;
 
-let computer = document.querySelector('.computer');
-let player = document.querySelector('.player');
+const computer = 1;
+const player = -1;
 
-let currentUser = 1;
+let currentUser;
 
-const playingBoard = document.querySelector('.grid div');
-playingBoard.addEventListener('click', function(){
-    alert(`click`);
-    return 
-});
+let playingBoard = [
+    [0, 0, 0, 0, 0, 0],  // column 0
+    [0, 0, 0, 0, 0, 0],  // column 1
+    [0, 0, 0, 0, 0, 0],  // column 2
+    [0, 0, 0, 0, 0, 0],  // column 3
+    [0, 0, 0, 0, 0, 0],  // column 4
+    [0, 0, 0, 0, 0, 0],  // column 5
+    [0, 0, 0, 0, 0, 0]  // column 6
+//   ]; document.querySelector('.grid div');
+];
+const rows = 6;
+const cols = 7;
 
 const rematchButton = document.querySelector('button');
-rematchButton.addEventListener('click', function(){
-    alert(`click`)
-    return newBoard;
-});
+rematchButton.addEventListener('click', init);
 
-const choiceEls = document.querySelectorAll('.grid-cell div');
+const winnerEl = document.querySelector('#declareWinner');
 
-const scoreEls = {
-    player: document.querySelector('#playerScore > h2'),
-    computer: document.querySelector('#computerScore > h2')
-};
+const playerChoice = document.querySelectorAll('.player');
+    playerChoice.forEach((choice, index) => {
+        choice.addEventListener('click', function(){
+            setPlayerChoice(index + 1)
+        })
+    });
 
-newGameSet(); // This loads your game to starting state (start screen)
-function newGameSet() {
+const computerChoice = document.querySelectorAll('.computer');
+    computerChoice.addEventListener('click', function(){
+        setComputerChoice(cpuThink());
+    });
+
+
+
+init(); // This loads your game to starting state (start screen)
+function init() {
     // controller function that sets the initial values of our Model
     // aka sets the initial values of the start game state
     scores = {
@@ -38,92 +48,82 @@ function newGameSet() {
       computer: 0
     };
   
-    choices = {
+    currentUser = {
       player: -1, 
-      computer: +1
+      computer: 1
     };
 
     
-    newBoard = [
+    playingBoard = [
         [0, 0, 0, 0, 0, 0],  // column 0
         [0, 0, 0, 0, 0, 0],  // column 1
         [0, 0, 0, 0, 0, 0],  // column 2
-        [0, 0, 0, 0, 0, 0],  // column 3
-        [0, 0, 0, 0, 0, 0],  // column 4
-        [0, 0, 0, 0, 0, 0],  // column 5
-        [0, 0, 0, 0, 0, 0]  // column 6
+        [0, 0, 0, 1, 0, 0],  // column 3
+        [0, 0, 1, 0, 0, 0],  // column 4
+        [0, 1, 0, 0, 0, 0],  // column 5
+        [1, 1, 1, 1, 0, 0]  // column 6
       ];
   
-    winner = null; // 'Player1', 'No Winner', or 'computer'
+    winner = null; // 'Player', 'Draw', or 'computer'
   
     // call the render function, to take the values of the state
     // and update the DOM visually representing the state
-    
+    render();
     
   };
 
-  function render(){
-    for (let key in newBoard){
-        console.log(key, " <- key");
-        [key].choiceEls.classList.add('player');// scores['player']
-        }
-   
-  };
+function render(){
+    
+    
+    };
 
- function reMatch(){
-    if (rematchButton.onclick){
-        return newGameSet;
-    }
+ 
+function reMatch(){
+   render(newBoard);
+
+};
+
+function setScore(){
+
 };
     
-  
-  
 
-
-  function playConnectFour(){
-    if(beginGame = true){
-        
-        currentUser.choice = setUserChoice();
-        return;
-    };
+function playConnectFour(){
+    beginGame = true;
+    currentUser.player[i] = setPlayerChoice();
+   
+    
     //controller function
     // it updates the state 
-    console.log('click happening')
-    // updating our state
-    // we are setting the results (choices of the player)
-    // to either 'rock', 'paper', or 'scissors'
-   
-
-    // Try to figure the Win LOGIC, update the score, and the winner variable
-    // use the results, in order to update the winner
     
-    // and then update the score in the approipate use
-
     
-
-    // at the end after you update state you always call render
-    // which takes the state variables (who won, score, img)
-    // and updates the DOM
-    render();
+    
 };
-playConnectFour();
 
 
-function setUserChoice(){
-    let playingBoard =  document.querySelector('.grid div');
+function setPlayerChoice(){
     for (let i = playingBoard.length - 1; i >= 0; i--) {
-        const choiceEls = board[i];
-     if (!choiceEls.classList.contains('filled')){
-        choiceEls.classList.add('taken');
-        choiceEls.classList.add('player');
+        const playerChoice = playingBoard[i];
+     if (!playerChoice.classList.contains('taken')){
+        playerChoice.classList.add('taken');
+        playerChoice.classList.add('player');
         } else {
             alert(`Cannot move here`);
         }
       }
       
     };
- 
-    setUserChoice();
+setPlayerChoice();
+
+function cpuThink(){
+    num = Math.floor(Math.random() * 7)
+        num += 1;
+    
+};
+
+function setComputerChoice(){
+
+};
 
 //     if(users.player.winList === true){
 //         console.log(`${users.player} wins!`)
